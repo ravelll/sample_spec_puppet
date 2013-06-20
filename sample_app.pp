1c9d66ad3a3bf58b5727ff407aaafc23a72cb99b
menchikatsu <menchikatsu@paperboy.co.jp>
2013-06-20 16:14:13 +0900

    adding dependency relationship between users and groups.

--- sample_app.pp
+++ sample_app.pp
@@ -1,6 +1,7 @@
 user { 'gussan':
   ensure => present,
   uid => 1000,
+  require => Group['app_user'],
   gid => ['app_user','rbenv'],
   comment => 'gussan',
   home => '/home/gussan',
@@ -10,6 +11,7 @@ user { 'gussan':
 
 group { 'app_user':
   ensure => present,
+  require => Group['rbenv'],
   gid => 1000,
 }
 
