62ad25eff6d395f78b6114d3af7188264a9463d0
menchikatsu <menchikatsu@paperboy.co.jp>
2013-06-20 14:29:08 +0900

    created puppet manifest for sample_app

---
+++ sample_app.pp
@@ -0,0 +1,18 @@
+user { 'gussan':
+  ensure => present,
+  uid => 1000,
+  gid => ['app_user','rbenv'],
+  comment => 'gussan',
+  home => '/home/gussan'
+  managehome => true,
+  shell => '/bin/bash',
+}
+
+group { 'app_user':
+  ensure => present,
+  gid => 1000,
+}
+
+group { 'rbenv':
+  ensure => present,
+}
