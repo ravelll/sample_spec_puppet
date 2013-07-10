08c9cb2b85fc953a375e15150bd8fdd54015596c
menchikatsu <menchikatsu@paperboy.co.jp>
2013-07-10 17:05:16 +0900

    Added resource to create .ssh directory

--- roles/common/manifests/ssh_key.pp
+++ roles/common/manifests/ssh_key.pp
@@ -1,4 +1,11 @@
 class common::ssh_key {
+  file { '/home/gussan/.ssh':
+    ensure => directory,
+    owner   => 'gussan',
+    group   => 'app_user',
+    mode    => '0700',
+  }
+
   file { '/home/gussan/.ssh/authorized_keys':
     content => template('common/ssh-key/id_rsa.pub'),
     owner   => 'gussan',
